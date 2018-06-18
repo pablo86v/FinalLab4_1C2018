386f8646d348ddaa295616397dddd8af588e6fd8
pablo86v <pablo86v@gmail.com>
2018-06-18 02:04:10 -0300

    +stored procedures

---
+++ spGetViajes.sql
@@ -0,0 +1,14 @@
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS spGetViajes$$
+CREATE PROCEDURE spGetViajes( )
+BEGIN
+	select  vi.idViaje,CONCAT(cl.idCliente," - ",us.apellido,",",us.nombre) as cliente, vi.idVehiculo,vi.fechaViaje,vi.monto,vi.estado
+	from tbViajes vi inner join tbClientes cl
+	on vi.idCliente = cl.idCliente
+	inner join tbUsuarios us on cl.idUsuario = us.idUsuario;
+	
+END$$
+
+DELIMITER ;
