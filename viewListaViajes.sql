e5683d98e239cc512061cd91cfc3477a1e937cf9
Pablo Valenzuela <pablo86v@gmail.com>
2018-06-20 18:32:02 -0300

    Delete viewListaViajes.sql

--- viewListaViajes.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE VIEW viewListaViajes
-AS
-	select  vi.idViaje,CONCAT(cl.idCliente," - ",us.apellido,",",us.nombre) as cliente, vi.idVehiculo,vi.fechaViaje,vi.monto,vi.estado
-	from tbViajes vi inner join tbClientes cl
-	on vi.idCliente = cl.idCliente
-	inner join tbUsuarios us on cl.idUsuario = us.idUsuario
-	order by vi.idViaje desc;
-
